9ead55184a086e56e99c86c3b9bbe268895a5683
jw941 <frank@baileyroberts.com.au>
2014-01-17 20:51:39 +1100

    Add home page slider.

---
+++ resources/script/index.js
@@ -0,0 +1,24 @@
+require(['dojo/dom',
+    'module/Slider',
+    'dojo/domReady!'],
+    function (dom, Slider) {
+        var node = dom.byId('slider');
+        node && new Slider({
+            layout: [
+                {
+                    href: 'http://www.google.com',
+                    target: '_blank',
+                    src: 'resources/images/image-slider-1.jpg',
+                    alt: 'Welcome to YouthClub'
+                },
+                {
+                    src: 'resources/images/image-slider-2.jpg',
+                    alt: ''
+                },
+                {
+                    src: 'resources/images/image-slider-3.jpg',
+                    alt: 'Pure Javascript. No jQuery. No flash.'
+                }
+            ]
+        }, node);
+    });
---
+++ resources/script/module/Slider.js
@@ -0,0 +1,38 @@
+define([
+    'dojo/_base/declare',
+    'dojo/dom',
+    'dojo/_base/array',
+    'dojo/dom-construct',
+    'dijit/_WidgetBase'
+], function (declare, dom, array, domConstruct, _WidgetBase) {
+
+    return declare([_WidgetBase], {
+
+        layout: null,
+
+        postCreate: function () {
+            this.inherited(arguments);
+            this.layout && this.createImages();
+        },
+
+        createImages: function () {
+            var self = this;
+            array.forEach(self.layout, function (item) {
+                var node = self.domNode;
+                if (item.href) {
+                    node = domConstruct.create('a', {
+                        href: item.href
+                    }, node);
+                    if (item.target) {
+                        node.target = item.target;
+                    }
+                }
+                domConstruct.create('img', {
+                    src: item.src,
+                    alt: item.alt
+                }, node);
+            });
+        }
+    });
+
+});
